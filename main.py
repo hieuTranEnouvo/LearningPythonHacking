5e7e1f2b0154472b20adbab6e8106fa1b45cb5cb
Hieu Tran <hieu.tran@enouvo.com>
2022-11-04 14:59:55 +0700

    fix options 2

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def getArguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="newMac", help="New MAC address")
-    (options, arguments) = parser.parse_args()
-    if not options.interface:
-        parser.error("[-] Please specify and interface, use --help for more information")
-    elif not options.newMac:
-        parser.error("[-] Please specify and Mac, use --help for more information")
-    return options
+    # (options, arguments) = parser.parse_args()
+    # if not options.interface:
+    #     parser.error("[-] Please specify and interface, use --help for more information")
+    # elif not options.newMac:
+    #     parser.error("[-] Please specify and Mac, use --help for more information")
+    # return options
+    return parser.parse_args()
 
 
 def changeMac(interface, newMac):
@@ -30,5 +31,15 @@ def changeMac(interface, newMac):
 
 # interface = options.interface
 # newMac = options.newMac
-options = getArguments()
+
+(options, argument) = getArguments()
 changeMac(options.interface, options.newMac)
+
+# print("[+] Changing MAC address for " + interface + " to " + newMac)
+# subprocess.call("ifconfig" + interface + " down", shell=True)
+# subprocess.call("ifconfig" + interface + " hw ether" + newMac, shell=True)
+# subprocess.call("ifconfig" + interface + " up", shell=True)
+
+# subprocess.call("ifconfig eth0 down", shell=True)
+# subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:77", shell=True)
+# subprocess.call("ifconfig eth0 up", shell=True)
