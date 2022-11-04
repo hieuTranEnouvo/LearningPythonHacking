cf7e23bc84216c399ad0655ca11f32c07f609a1d
Hieu Tran <hieu.tran@enouvo.com>
2022-11-04 16:34:18 +0700

    fix str

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def changeMac(interface, newMac):
 
 def getCurrentMac(interface):
     # enter the line (\n)
-    ifconfig_result = bytes(subprocess.check_output(["ifconfig", interface]))
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
     print(ifconfig_result)
     # get address eth0
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
