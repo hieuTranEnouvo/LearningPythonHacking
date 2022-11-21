3148f2a30f3107f8b1dd64b6953f10841bae6224
Hieu Tran <hieu.tran@enouvo.com>
2022-11-21 10:06:29 +0700

    hash

---
+++ hash.py
@@ -0,0 +1,7 @@
+import hashlib
+import sys
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("hash Cracker")
+print(ascii_banner)
+print("Algorithms available: MD5 | SHA1 | SHA224 | SHA512 | SHA224 | SHA384")
